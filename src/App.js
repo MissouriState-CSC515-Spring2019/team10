912b0cdcdaa3ab19001245ec919455195e20fe1f
HaileyM12 <haileymartinelli@yahoo.com>
2019-04-18 21:20:50 -0500

    Installed react-router-dom and imported Route, NavLink, and HashRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
 // import './App.css';
+import {
+	Route,
+	NavLink,
+	HashRouter
+} from 'react-router-dom';
 import Header from './components/header.js';
 import Navigation from './components/navigation.js';
 import Footer from './components/footer.js';
 import SunContent from './components/sun.js';
-// import MoonContent from './components/moon.js';
-// import EclipseContent from './components/eclipse.js';
+import MoonContent from './components/moon.js';
+import EclipseContent from './components/eclipse.js';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="container">
-				<Header/>
-				<Navigation/>
-				<SunContent/>
-				<Footer/>
-			</div>
+			<HashRouter>
+				<div className="container">
+					<Header/>
+					<Navigation/>
+					<Footer/>
+		
+					<div className="content">
+						<Route exact path="/" component={SunContent}/>
+						<Route path="/moon" component={MoonContent}/>
+						<Route path="/eclipse" component={EclipseContent}/>
+					</div>
+				</div>
+			</HashRouter>
 		);
 	}
 }
