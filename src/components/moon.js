271bba488ee54b2a55e3afb73a8064cbe4256e50
Carmen Creswell <ccreswell@adaptavist.com>
2019-04-21 22:19:05 -0500

    Add moonrise and moonset time

--- src/components/moon.js
+++ src/components/moon.js
@@ -1,16 +1,51 @@
 import React, {Component} from 'react';
 
 class MoonContent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			results: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://api.usno.navy.mil/rstt/oneday?date=01/01/2019&loc=Springfield,%20Mo")
+		  .then(res => res.json())
+		  .then(
+			(results) => {
+			  this.setState({
+				isLoaded: true,
+				results: results
+			  });
+			},
+			(error) => {
+			  this.setState({
+				isLoaded: true,
+				error
+			  });
+			}
+		  )
+	}
+	
 	render() {
-		return (
-			<div className="content">
-				<div id="sunrise">Moon Rise: TIME</div>
-				<div id="moonImage">
-					<div id="moonPhase">PHASE</div>
+		const { error, isLoaded, results} = this.state;
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div className="content">
+					<div id="sunrise">Moon Rise: {results.moondata[0].time}</div>
+					<div id="moonImage">
+						<div id="moonPhase">PHASE</div>
+					</div>
+					<div id="sunset">Moon Set: {results.moondata[2].time}</div>
 				</div>
-				<div id="sunset">Moon Set: TIME</div>
-			</div>
-		);
+			);
+		}
 	}
 }
 
