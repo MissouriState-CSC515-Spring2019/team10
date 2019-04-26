92f7639f1af6518d4d8dcbe8100c08ba770fdd11
Kristopher Kvenvold <kvenvold001@live.missouristate.edu>
2019-04-26 11:22:26 -0500

    Group Project Updated moon.js to add a date to set time if it was on the next day.

--- src/components/moon.js
+++ src/components/moon.js
@@ -108,6 +108,17 @@ class MoonContent extends Component {
 					moonSet = results.moondata[i].time.toString().slice(0, -3)
 				}
 			}
+
+			let moonSetNextDay;
+
+			 if (moonSet.slice(-4) === "a.m.") {
+			 	let nextDay = new Date(results.year,Number(results.month-1),results.day);
+			 	nextDay.setDate(nextDay.getDate()+1);
+			 	moonSetNextDay = '(' + Number(nextDay.getMonth()+1) + '/' + nextDay.getDate() + ')';
+			 } else {
+			 	moonSetNextDay = "";
+			 }
+
 			return (
 				<div className="contentNight">
 					<div id="moonRise">
@@ -118,7 +129,7 @@ class MoonContent extends Component {
 						<div id="moonPhase">{moonPhase}</div>
 					</div>
 					<div id="moonSet">
-						Moon Set: {moonSet}
+						Moon Set: {moonSet} {moonSetNextDay}
 					</div>
 				</div>
 			);
